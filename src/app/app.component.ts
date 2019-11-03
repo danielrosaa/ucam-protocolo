dbb0d6af7d7fa39f3b5d710fd40e17d3bbc52981
Daniel Rosa <danielrosaa@gmail.com>
2019-11-02 23:13:35 -0300

    Configura validação de form e inserção na lista.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,55 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  descricao = ''
-  protocolo = [
-    {
-      "id": 1,
-      "name": "Trancamento/Cancelamento",
-      "subtitulo": "12 tipos de natureza",
-      "unidade": 43,
-      "tipos_natureza": 234,
-      "active": false
-    },
-    {
-      "id": 2,
-      "name": "Valores pagos",
-      "subtitulo": "12 tipos de natureza",
-      "unidade": 12,
-      "tipos_natureza": 234,
-      "active": true
-    },
-    {
-      "id": 3,
-      "name": "Declaração",
-      "subtitulo": "12 tipos de natureza",
-      "unidade": 12,
-      "tipos_natureza": 234,
-      "active": true
-    },
-    {
-      "id": 4,
-      "name": "Declaração",
-      "subtitulo": "12 tipos de natureza",
-      "unidade": 12,
-      "tipos_natureza": 234,
-      "active": true
-    },
-    {
-      "id": 5,
-      "name": "Declaração",
-      "subtitulo": "12 tipos de natureza",
-      "unidade": 12,
-      "tipos_natureza": 234,
-      "active": true
-    }
-  ]
+export class AppComponent implements OnInit {
+  protocolo: object[];
+  form = new FormGroup({
+    descricao: new FormControl('', Validators.required),
+    input: new FormControl('', Validators.required)
+  })
 
-  log(x) {
-    console.log(x)
+  ngOnInit() {
+    this.protocolo = [];
+  }
+
+  addItem(item) {
+    console.log(this.form)
+    if (!this.form.get('descricao').invalid) {
+      this.protocolo.push({
+          "name": item.value,
+          "subtitulo": "12 tipos de natureza",
+          "unidade": 12,
+          "tipos_natureza": 234
+        })
+      this.clearForm()
+    } else {
+      this.form.setErrors({
+        fieldsEmpty: true
+      })
+    }
+  }
+
+  clearForm() {
+    this.form.reset();
+  }
+
+  get descricao() {
+    return this.form.get('descricao')
+  }
+  get input() {
+    return this.form.get('input')
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
+import { ListaComponent } from './lista/lista.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    HeaderComponent
+    HeaderComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
