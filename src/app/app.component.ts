151b8daebd23e167f0ea1c954ac6de3a2f1c57cb
Daniel Rosa <danielrosaa@gmail.com>
2019-11-03 01:06:29 -0300

    Editar lista.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,24 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  protocolo: object[];
+  protocolo: Protocolo[];
+  desc: HTMLInputElement;
+  novaNatureza: HTMLButtonElement;
+  editar: boolean;
+  indexProtocolo: number;
   form = new FormGroup({
     descricao: new FormControl('', Validators.required),
     input: new FormControl('', Validators.required)
   })
 
   ngOnInit() {
-    this.protocolo = [];
+    this.protocolo = []
+    this.editar = false
   }
-
   addItem(item) {
-    console.log(this.form)
     if (!this.form.get('descricao').invalid) {
       this.protocolo.push({
-          "name": item.value,
+          "nome": item.value,
           "subtitulo": "12 tipos de natureza",
           "unidade": 12,
           "tipos_natureza": 234
@@ -37,10 +40,32 @@ export class AppComponent implements OnInit {
     this.form.reset();
   }
 
+  editItem(index: number, desc: HTMLInputElement, botao: HTMLButtonElement) {
+    desc.focus()
+    this.editar = true
+    this.indexProtocolo = index
+    desc.value = this.protocolo[index].nome
+    // this.protocolo[index].nome = this.form.get('descricao').value
+    // this.protocolo.splice(index)
+  }
+
+  updateProtocolo() {
+    this.protocolo[this.indexProtocolo].nome = this.form.get('descricao').value
+    this.editar = false
+    this.clearForm()
+  }
+
   get descricao() {
     return this.form.get('descricao')
   }
   get input() {
     return this.form.get('input')
   }
+}
+
+interface Protocolo {
+  nome: string,
+  subtitulo: string,
+  unidade: number,
+  tipos_natureza: number
 }
