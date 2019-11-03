4826506ae952e00a1a803951d1b5e4de201ab8e5
Daniel Rosa <danielrosaa@gmail.com>
2019-11-03 18:32:40 -0300

    Mais testes.

--- src/app/lista/lista.component.spec.ts
+++ src/app/lista/lista.component.spec.ts
@@ -6,7 +6,7 @@ describe('ListComponent', () => {
     component = new ListaComponent()
   })
 
-  it('testa se evento para editar é chamado', () => {
+  it('testa se evento para editar é chamado a partir do componente', () => {
     let index = null
     component.edit.subscribe(value => index = value)
 
@@ -15,7 +15,16 @@ describe('ListComponent', () => {
     expect(index).toBe(1)
   })
 
-  it('testa se evento para deletar é chamado', () => {
+  it('testa se evento para deletar é chamado a partir do componente', () => {
+    let index = null
+    component.delete.subscribe(value => index = value)
+
+    component.eventDelete(1)
+
+    expect(index).toBe(1)
+  })
+
+  it('testa se botão "Deletar" exclui item da lista', () => {
     let index = null
     component.delete.subscribe(value => index = value)
 
